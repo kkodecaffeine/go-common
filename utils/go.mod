6493448de925d65ac3b36a723e2bfd04decb24f0
kkodecaffeine <kkodecaffeine@gmail.com>
2022-12-27 20:46:29 +0900

    fix

--- utils/go.mod
+++ utils/go.mod
@@ -9,8 +9,15 @@ require (
 )
 
 require (
+	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
+	golang.org/x/sys v0.0.0-20210806184541-e5e7981a1069 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
+
+require (
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/go-playground/validator/v10 v10.11.1
 	github.com/leodido/go-urn v1.2.1 // indirect
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 )
--- utils/pattern.go
+++ utils/pattern.go
@@ -1,9 +1,10 @@
 package utils
 
 import (
+	"reflect"
 	"regexp"
 
-	"github.com/go-playground/validator"
+	"github.com/go-playground/validator/v10"
 )
 
 const phoneRegex = `^01([0|1|6|7|8|9])([0-9]{3,4})([0-9]{4})$`
@@ -13,11 +14,12 @@ func ValidateRegex(regex, value string) bool {
 	return reg.Match([]byte(value))
 }
 
-func RegexPhone() validator.Func {
-	return func(fl validator.FieldLevel) bool {
-		if value, ok := fl.Field().Interface().(string); ok {
-			return ValidateRegex(phoneRegex, value)
-		}
-		return true
+func RegexPhone(
+	v *validator.Validate, topStruct reflect.Value, currentStructOrField reflect.Value,
+	field reflect.Value, fieldType reflect.Type, fieldKind reflect.Kind, param string,
+) bool {
+	if value, ok := field.Interface().(string); ok {
+		return ValidateRegex(phoneRegex, value)
 	}
+	return true
 }
