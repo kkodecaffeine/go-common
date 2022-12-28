e33c5c712ab4a0feb7d37c7fa23e376d9df44c0b
kkodecaffeine <kkodecaffeine@gmail.com>
2022-12-28 18:41:44 +0900

    fix: update import path

--- middleware/token/token.middleware.go
+++ middleware/token/token.middleware.go
@@ -4,12 +4,12 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/kkodecaffeine/go-common/utils/token"
+	"github.com/kkodecaffeine/go-common/utils"
 )
 
 func JwtAuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		err := token.ValidToken(c)
+		err := utils.ValidToken(c)
 		if err != nil {
 			c.String(http.StatusUnauthorized, "Unauthorized")
 			c.Abort()
